02e8ef9eec93d772561c366a8f6906065f3f29a5
MinHyuk-github <alsgurdl258@gmail.com>
2022-03-24 16:26:13 +0900

    update(03-24)

--- src/main/java/mvc/controller/DeptController.java
+++ src/main/java/mvc/controller/DeptController.java
@@ -17,25 +17,32 @@ public class DeptController {
 	@Autowired
 	private DeptService service;
 	
+	
 	// 부서문서 리스트 
 	// http://localhost:7080/borampms/dept.do?method=list
 	@RequestMapping(params="method=list")
 	public String ddList(DeptDocSch sch, Model d) {
 		d.addAttribute("ddList", service.getDDList(sch));
 		
-		return "dept\\Doc-Management";
+		return "dept\\DeptList";
 	}
-	
+	// http://localhost:7080/borampms/dept.do?method=insertFrm
+	// 초기 화면 호출
+	@RequestMapping(params="method=insertFrm")
+	public String ddInsertFrm(DeptDoc Dept) { 
+			
+		return "dept\\DeptInsert";
+	}
 	// 부서문서 등록
-	// http://localhost:7080/borampms/dept.do?method=insert
 	@RequestMapping(params="method=insert")
-	public String boardInsertFrm(DeptDoc ins, Model d) {
+	public String ddInsertFrm(DeptDoc ins, Model d) {
 		System.out.println("확인:"+ins.getDdTitle());
 		System.out.println("확인:"+ins.getDdWriter());
 		System.out.println("확인:"+ins.getDdDept());
 		System.out.println("확인:"+ins.getDdContent());
 		System.out.println("확인:"+ins.getReport());
 		d.addAttribute("msg",service.insertDD(ins));
-		return "redirect:/dept.do?method=list"; 
+		return "dept\\DeptInsert"; 
 	}
+	
 }
