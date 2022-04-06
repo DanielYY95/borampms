d6fbd7c8577a813650d0ace72f91941aabbc268f
Daniel_Yang <84573015+DanielYY95@users.noreply.github.com>
2022-04-06 19:32:52 +0900

    Update CommonDocSch.java

--- src/main/java/mvc/vo/CommonDocSch.java
+++ src/main/java/mvc/vo/CommonDocSch.java
@@ -14,6 +14,9 @@ public class CommonDocSch {
 	
 	// 검색할 내용
 	private	String cdWriter; // 작성자 검색
+	
+	// 프로젝트 세션값
+	private String piId;
 
 	public CommonDocSch() {
 		super();
@@ -108,6 +111,14 @@ public class CommonDocSch {
 
 	public void setCdWriter(String cdWriter) {
 		this.cdWriter = cdWriter;
+	}
+
+	public String getPiId() {
+		return piId;
+	}
+
+	public void setPiId(String piId) {
+		this.piId = piId;
 	}
 	
 }
